42c15a019ebf3dad22557b3f809997739ebc9d98
Rifqi-Achmad <Rifqihallo@gmail.com>
2024-09-24 14:34:47 +0700

    fix app.js

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.get('/rifqi', (req, res) => {
 
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
-  connection.query(sql,(fields) => {
+  connection.query(sql,(error, fields) => {
+    res.send(fields)
   })
 });
 
